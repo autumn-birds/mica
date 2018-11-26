e17e37643f5b611835ed555e216a97be4e829187
curiousbirds <curiousbirds@users.noreply.github.com>
2018-11-25 18:38:19 -0800

    Add basic login; change some default texts

--- mica.py
+++ mica.py
@@ -10,14 +10,25 @@ logging.basicConfig(level=logging.INFO)
 
 db = None
 
+# Default messages that would otherwise be hard-coded about in the code.
+texts = {
+    'welcome': "Welcome.  Type `connect <name> <password>' to connect.",
+    'badLogin': "I'm sorry, but those credentials were not right.",
+    'cmd404': "I don't understand what you mean.",
+    'thing404': "I can't find any such thing as `%s'.",
+    'cmdSyntax': "That command needs to be written similar to `%s'.",
+}
+
+
 def setup_db(database):
     dbSetup = [
       "CREATE TABLE things (id INTEGER PRIMARY KEY, owner_id INTEGER, location_id INTEGER, name TEXT, desc TEXT)",
       "CREATE TABLE accounts (id INTEGER PRIMARY KEY, character_id INTEGER, password TEXT)",
       "CREATE TABLE links (id INTEGER PRIMARY KEY, name TEXT, from_id INTEGER, to_id INTEGER)",
       
-      "INSERT INTO things (id, owner_id, location_id, name, desc) VALUES (1, 1, 2, \"One\", \"A nameless, faceless, ageless, gender-neutral, culturally ambiguous embodiment of... it's not clear, really.  Ultimate power over reality for one thing, but they don't seem to be trying to show this off.\")",
-      "INSERT INTO things (id, owner_id, location_id, name, desc) VALUES (2, 1, 2, \"Nexus\", \"It is a place: that is about all that can be ascertained.\")",
+      "INSERT INTO things (id, owner_id, location_id, name, desc) VALUES (1, 1, 2, \"One\", \"A nameless, faceless, ageless, gender-neutral, culturally ambiguous embodiment of... well, it's not clear, really.\")",
+      "INSERT INTO things (id, owner_id, location_id, name, desc) VALUES (2, 1, 2, \"Nexus\", \"It is a place: that is about all you can be sure of.\")",
+      # TODO: Password hashing
       "INSERT INTO accounts (character_id, password) VALUES (1, \"potrzebie\")"
     ]
 
@@ -45,6 +56,18 @@ def from_db(query, opts=None):
     """Return the results from fetchall() of a database query; just for convenience."""
     return calldb(query, opts).fetchall()
 
+def find_account(account):
+    """Return a tuple of (password, objectID) for the given account name, if it exists."""
+    candidates = from_db("SELECT id FROM things WHERE name=? AND id IN (SELECT character_id FROM accounts)", (account,))
+    if len(candidates) != 1:
+        return None
+
+    acct = from_db("SELECT password, character_id FROM accounts WHERE id=?", (candidates[0][0],))
+    if len(acct) == 0:
+        return None
+    assert(len(acct) == 1)
+    return acct[0]
+
 def describe_thing(id, get_in=True):
     """Call up the database and return the name and desc of a Thing along with its contents (any Things with a location_id of the requested Thing.)
     The form is a tuple: name, descs, contents (or if get_in=False, name, desc.)
@@ -53,7 +76,7 @@ def describe_thing(id, get_in=True):
     assert len(thing) <= 1
     if len(thing) == 0:
         return None
-    
+
     if not get_in:
         return (thing[0][0], thing[0][1])
 
@@ -80,6 +103,7 @@ del _opts
 db = sqlite3.connect(":memory:")
 setup_db(db)
 
+
 # Commands that can be run.
 commands = {}
 # The abstract notion of indexing a network link to a state of being either not logged in at all or connected to some object in the database.
@@ -90,24 +114,59 @@ client_states = {}
 # 2. They will support a write() method which takes a chunk of UTF-8 encoded text in a single argument and writes it onto the link; this method will do any buffering, etc., that is required, only giving up if the link itself fails.
 # 3. They will support a kill() method that forcefully disconnects the client on the other end of the link.
 
+commands = []
+
 def line(text):
     """Encapsulates the process of adding a proper newline to the end of lines, just in case it ever needs to be changed."""
     return text + "\r\n"
 
 def on_connection(new_link):
-    client_states[new_link] = -1
-    new_link.write("Welcome.  Type `connect <name> <password>' to connect.\n")
+    client_states[new_link] = {'character': -1}
+    new_link.write(line(texts['welcome']))
     logging.info("Got link " + repr(new_link))
 
 def on_text(link, text):
     assert link in client_states
-    # TODO
-    link.write(line(text))
+    s = client_states[link]['character']
+
+    if s == -1:
+        cmd = "connect"
+        print("On connect, text=%s and clipped, %s" % (text, text[:len(cmd)]))
+        if text[:len(cmd)] == cmd:
+            try_login(link, text[len(cmd)+1:])
+        else:
+            link.write(line(texts['cmd404']))
+        return
+
+    for cmd in commands:
+        if cmd[0] == text[:len(cmd[0])]:
+            cmd[1](text[len(cmd[0])+1:], link)
+            return
+
+    link.write(line(texts['cmd404']))
 
 def on_disconnection(old_link):
     assert old_link in client_states
     del client_states[old_link]
     logging.info("Losing link " + repr(old_link))
+
+def try_login(link, args):
+    # TODO: allow double-quote parsing? Maybe?
+    print("try_login: got %s" % args)
+    args = args.split(' ')
+    if len(args) != 2:
+        link.write(line(texts['cmdSyntax'] % "connect <username> <password>"))
+        return
+    acct = find_account(args[0])
+
+    # Again, TODO: Password hashing
+    if acct[0] == args[1]:
+        assert describe_thing(acct[1]) is not None    # TODO: Is this really necessary? It might be a significant performance drop (more database calls, even more if the object has a lot of objects in it), which could matter since malicious users can try to sign in very frequently, and they don't need to authenticate themselves first (duh.)
+        client_states[link]['character'] = acct[1]
+        return True
+    else:
+        link.write(line(texts['badLogin']))
+        return False
 
 
 # The horrible, ugly world of networking code.
